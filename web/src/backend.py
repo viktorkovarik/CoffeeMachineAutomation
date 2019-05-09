3e453801bade37403a55892faecd9ab84acc2542
viktorkovarik <viktorkovarik@gmail.com>
2019-05-09 17:32:44 +0200

    Changed to port 80

--- web/src/backend.py
+++ web/src/backend.py
@@ -383,7 +383,7 @@ class Server(BaseHTTPRequestHandler):
 
 
         
-def run(server_class=HTTPServer, handler_class=Server, port=8080):
+def run(server_class=HTTPServer, handler_class=Server, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
 
